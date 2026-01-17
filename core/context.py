171433c1d4896d8d0dbc318c71c30f9f75291b54
QnK1 <kacperro03@gmail.com>
2026-01-17 19:44:37 +0100

    Reverted contextvar change

--- core/context.py
+++ core/context.py
@@ -1,26 +1,11 @@
-import threading
-from typing import Any
+from contextvars import ContextVar
+
 from access_control.strategies.WhitelistStrategy import WhitelistStrategy
 
 
-class ThreadVar:
-    def __init__(self, name: str, default=None) -> None:
-        self.name: str = name
-        self._state: Any = default
-        self._lock: threading.Lock = threading.Lock()
-    
-    def set(self, value) -> None:
-        with self._lock:
-            self._state = value
-    
-    def get(self) -> Any:
-        with self._lock:
-            return self._state
-
-
-_current_user = ThreadVar("els_current_user", default=None)
-_current_session = ThreadVar("els_current_session", default=None)
-_current_base = ThreadVar("els_current_base", default=None)
-_current_strategy = ThreadVar("els_current_strategy", default=WhitelistStrategy())
-_filters_activated = ThreadVar("els_filters_activated", default=False)
+_current_user = ContextVar("els_current_user", default=None)
+_current_session = ContextVar("els_current_session", default=None)
+_current_base = ContextVar("els_current_base", default=None)
+_current_strategy = ContextVar("els_current_strategy", default=WhitelistStrategy())
+_filters_activated = ContextVar("els_filters_activated", default=False)
 
